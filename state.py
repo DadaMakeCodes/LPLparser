c35ce09674d87b1f670ccdb86b9e38f74e2cca54
Dada <dadabloob@gmail.com>
2026-02-05 18:04:27 +0100

    Update state.py

--- state.py
+++ state.py
@@ -12,10 +12,31 @@ Mantilex = [[0.0, 0.0, 1.0],
      [0.0, 1.0, 0.0],
      [1.0, 0.0, 0.0],]
 
-M = Mdeglex 
+M = []
 
 
 def lexGraph() -> list[list[float]]:
     n = len(ringvar)
     return [[1.0 if i == j else 0.0 for j in range(n)] for i in range(n)]
 
+def Matrix_Prod(A: list[list[float]], B: list[list[float]]) -> list[list[float]]:
+    return (np.array(A) @ np.array(B)).tolist()
+
+def Matrix_degLex() -> list[list[float]]:
+    n = len(ringvar)
+    M = []
+    for i in range(n):
+        row = [1.0] + [1.0 if i == j else 0.0 for j in range(n)]
+        M.append(row)
+    return M
+
+def Matrix_AntiLex() -> list[list[float]]:
+    n = len(ringvar)
+    M = []
+    for i in range(n):
+        row = [1.0 if j == (n - 1 - i) else 0.0 for j in range(n)]
+        M.append(row)
+    return M
+
+def Matrix_AntiDegLex() -> list[list[float]]:
+    return Matrix_Prod( Matrix_AntiLex(), Matrix_degLex())
